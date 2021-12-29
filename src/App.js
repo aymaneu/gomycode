df0401d8a5fe8f592b09e0d1fedcf4f32a718b0f
aymaneu <aymanelgad95@gmail.com>
2021-12-29 15:41:21 +0100

    gg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import "./App.css";
-import Adress from "./profile/Adress";
-import FullName from "./profile/FullName";
-import ProfilePhoto from "./profile/ProfilePhoto";
-import "./Style.css";
-function App() {
-  return (
-    <div className="App">
-      <ProfilePhoto />
-      <FullName />
-      <Adress />
-    </div>
-  );
+import React from "react";
+
+import Profil from "./profil/Profil";
+
+class App extends React.Component {
+  render() {
+    const myVar = {
+      fullName: "ayman elgad",
+      profession: " student",
+      bio: "i like to work in project",
+    };
+    return (
+      <div>
+        <Profil data={myVar} style={{ display: "flex" }}>
+          <img
+            src="https://image.flaticon.com/icons/png/512/1499/1499575.png"
+            width="200px"
+            height="200px"
+          ></img>
+        </Profil>
+      </div>
+    );
+  }
 }
 
 export default App;
